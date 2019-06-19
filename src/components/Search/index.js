7e090318b5adcb0b2152467fa5771ab57efd2bfb
leireriel <leirericop@gmail.com>
2019-06-19 17:27:06 +0200

    Create styles for search form

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,21 +1,21 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './Search.scss';
 
 class Search extends Component {
   render() {
     const { actionForm } = this.props;
     return(
-      <Fragment>
+      <div className="search__character--wrapper">
         <form action="" method="get" id="formCharacter">
-          <label htmlFor="search">
+          <label htmlFor="search" className="search__label--character">
             Enter name of a Marvel character
           </label>
-          <input type="text" id="search" placeholder="Character Name"/>
+          <input type="text" id="search" placeholder="Character Name" className="search__input--character"/>
         </form>
-        <button type="submit" form="formCharacter" onClick={actionForm}>
+        <button type="submit" form="formCharacter" className="search__button--character" onClick={actionForm}>
             <i className="fas fa-search"></i>
         </button>
-      </Fragment>
+      </div>
     );
   }
 }
