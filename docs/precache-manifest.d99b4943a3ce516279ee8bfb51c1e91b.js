9417e109ea6316a43da58b12596dbd6c1fe628bb
leireriel <leirericop@gmail.com>
2019-06-21 23:26:48 +0200

    Publish proyect

---
+++ docs/precache-manifest.d99b4943a3ce516279ee8bfb51c1e91b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4f9340f5bc077b52b4ba9d6e378629",
+    "url": "./index.html"
+  },
+  {
+    "revision": "985b0fe4b5c34b7415b3",
+    "url": "./static/css/main.5ff1691a.chunk.css"
+  },
+  {
+    "revision": "49f4c14efc2b990120e9",
+    "url": "./static/js/2.6bb32633.chunk.js"
+  },
+  {
+    "revision": "985b0fe4b5c34b7415b3",
+    "url": "./static/js/main.cb9eab55.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
